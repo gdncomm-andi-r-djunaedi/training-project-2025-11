1b2cc337e44a06e8a87e406802e3dbecf27878a6
Harikrishnan B <harikrishnanb@Harikrishnans-MacBook-Pro-2.local>
2025-12-01 17:28:05 +0530

    Initial code push

---
+++ member/src/test/java/com/blibi/member/MemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blibi.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
