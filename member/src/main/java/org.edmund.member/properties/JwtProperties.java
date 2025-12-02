b9daf03036778a4e1c2dd40a0204122c69508220
Edmundus <edmundus.prasetyaji@gdn-commerce.com>
2025-12-02 14:46:21 +0700

    part1d- Make JWT Props

---
+++ member/src/main/java/org.edmund.member/properties/JwtProperties.java
@@ -0,0 +1,13 @@
+package org.edmund.member.properties;
+
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "jwt-secret")
+@Data
+public class JwtProperties {
+    private String secret;
+}
