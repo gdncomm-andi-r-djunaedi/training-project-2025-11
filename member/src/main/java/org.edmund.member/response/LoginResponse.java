14711616189818621f84c1550c9ea235a5aab03a
Edmundus <edmundus.prasetyaji@gdn-commerce.com>
2025-12-02 14:48:08 +0700

    part1g- make response template

---
+++ member/src/main/java/org.edmund.member/response/LoginResponse.java
@@ -0,0 +1,15 @@
+package org.edmund.member.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginResponse {
+    private String token;
+    private String type = "Bearer";
+}
