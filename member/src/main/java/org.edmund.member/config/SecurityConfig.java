b134983b74aedd5fb34c05b06d393cd979c00f08
Edmundus <edmundus.prasetyaji@gdn-commerce.com>
2025-12-02 14:45:57 +0700

    part1h- make security configurations

---
+++ member/src/main/java/org.edmund.member/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package org.edmund.member.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
+import org.edmund.commonlibrary.security.*;
+import org.edmund.member.properties.JwtProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@RequiredArgsConstructor
+@Configuration
+public class SecurityConfig {
+    private final JwtProperties jwtProperties;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public JwtService jwtService() {
+        return new JwtService(jwtProperties.getSecret());
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/members/**").permitAll()
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .anyRequest().authenticated());
+        return http.build();
+    }
+
+}
