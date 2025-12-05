8becbb729a90e590ef3d487ec410efb3905b8365
gdncomm-verix-visarada <verix.visarada@gdn-commerce.com>
2025-12-06 01:33:51 +0700

    - implemented cart

---
+++ cart/src/main/java/com/training/marketplace/cart/client/ProductClientImpl.java
@@ -0,0 +1,18 @@
+package com.training.marketplace.cart.client;
+
+import com.training.marketplace.product.controller.modal.request.GetProductDetailRequest;
+import com.training.marketplace.product.controller.modal.request.GetProductDetailResponse;
+import com.training.marketplace.product.service.ProductServiceGrpc;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.grpc.client.ImportGrpcClients;
+
+@ImportGrpcClients
+public class ProductClientImpl {
+
+    @Autowired
+    ProductServiceGrpc.ProductServiceBlockingStub stub;
+
+    public GetProductDetailResponse getProductDetail(String productId){
+        return stub.getProductDetail(GetProductDetailRequest.newBuilder().setProductId(productId).build());
+    }
+}
